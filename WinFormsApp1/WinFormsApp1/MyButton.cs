7454f67e468745b86324394f465c66016a0c4a9e
André <AndreR@ppedv.de>
2023-03-08 12:21:48 +0100

    WinForms Template Pattern MyButton

---
+++ WinFormsApp1/WinFormsApp1/MyButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class MyButton : Button
+    {
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            pevent.Graphics.FillRectangle(Brushes.Gray, ClientRectangle);
+            pevent.Graphics.FillEllipse(Brushes.CadetBlue, ClientRectangle);
+        }
+    }
+}
