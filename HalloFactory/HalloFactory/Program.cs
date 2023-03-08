f17dd6366058081bccf2c5714eb1fcf6406e547d
André <AndreR@ppedv.de>
2023-03-08 10:15:18 +0100

    Factory

---
+++ HalloFactory/HalloFactory/Program.cs
@@ -0,0 +1,24 @@
+﻿using Mysqlx.Notice;
+using System.Data.Common;
+
+Console.WriteLine("Hello, World!");
+
+var sqlServerConString = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=true";
+
+//DbProviderFactory factory = Microsoft.Data.SqlClient.SqlClientFactory.Instance;
+DbProviderFactory factory = MySql.Data.MySqlClient.MySqlClientFactory.Instance;
+
+
+var con = factory.CreateConnection();
+con.ConnectionString = sqlServerConString;
+con.Open();
+
+var cmd = factory.CreateCommand();
+cmd.Connection= con;
+cmd.CommandText = "SELECT COUNT(*) FROM sys.all_objects";
+
+var count = cmd.ExecuteScalar();
+
+Console.WriteLine($"{count} Einträge in all_objects");
+
+
