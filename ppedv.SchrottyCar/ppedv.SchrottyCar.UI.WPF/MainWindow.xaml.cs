6349d91efcaaf1a27159cdd911c754be079a1063
André <AndreR@ppedv.de>
2023-03-09 16:58:02 +0100

    WPF mit Adonis UI

--- ppedv.SchrottyCar/ppedv.SchrottyCar.UI.WPF/MainWindow.xaml.cs
+++ ppedv.SchrottyCar/ppedv.SchrottyCar.UI.WPF/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace ppedv.SchrottyCar.UI.WPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
